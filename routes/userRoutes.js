5c336d3fe2f5056092a4210bf0f828f33cfc0dbf
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2026-04-24 16:12:44 +0530

    authentication part done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/signup", async (req, res) => {
     const { name, email, password } = req.body;
@@ -30,6 +32,47 @@ router.post("/signup", async (req, res) => {
         console.log(error);
         res.status(500).json({ message: "Server error" });
     }
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        // 🔥 TOKEN GENERATE
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token: token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.get("/profile", authMiddleware, (req, res) => {
+    res.json({
+        message: "Protected data",
+        user: req.user
+    });
 });
 
 module.exports = router;
