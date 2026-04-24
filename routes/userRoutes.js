1c4b317fab85498de525a9393c951dce39e2fc5e
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2026-04-24 13:01:09 +0530

    password hashing by using bcrypt.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.post("/signup", async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        // 👇 check existing user
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
         }
 
-        const user = new User({ name, email, password });
-        await user.save();
+        // 🔐 hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        console.log("Saving user:", name, email);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        await user.save();
 
         res.status(201).json({ message: "User registered successfully" });
 
